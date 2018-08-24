24d986fecb590614f5499c0debf5a2c9997833f1
Administrator <zhaowanyue1118@163.com>
2018-08-24 08:22:03 +0800

    aaa

--- src/com/xupt/edu/bishi/lianjia/mianshi/InsertSort.java
+++ src/com/xupt/edu/bishi/lianjia/mianshi/InsertSort.java
@@ -6,4 +6,25 @@ package com.xupt.edu.bishi.lianjia.mianshi;
  * @description:
  */
 public class InsertSort {
+    private void sort(int[] a) {
+        for(int i=1;i<a.length;i++)
+        {
+            int insertData=a[i];
+            int j=i-1;
+            while(j>=0 && insertData<a[j])
+            {
+                a[j+1]=a[j];
+                j--;
+            }
+            a[j+1]=insertData;
+        }
+    }
+    public static void main(String[] args) {
+        int a[]={5,4,3,2,1};
+        new InsertSort().sort(a);
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
 }
