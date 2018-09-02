429f4c2b4f930208cc8847530d9de5bfe30549d7
Administrator <zhaowanyue1118@163.com>
2018-09-03 07:56:10 +0800

    aaa

---
+++ src/com/xupt/edu/bishi/haoweilai/BubbleSort.java
@@ -0,0 +1,17 @@
+package com.xupt.edu.bishi.haoweilai;
+
+/**
+ * @author: zhaowanyue
+ * @date: 2018/8/28
+ * @description:
+ */
+public class BubbleSort {
+    public void sort(int a[])
+    {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/xupt/edu/bishi/haoweilai/ShengXuHe.java
@@ -0,0 +1,27 @@
+package com.xupt.edu.bishi.haoweilai;
+
+import java.util.Scanner;
+
+/**
+ * @author: zhaowanyue
+ * @date: 2018/8/28
+ * @description:
+ */
+public class ShengXuHe {
+//    public int max(int []array)
+//    {
+//
+//
+//    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int array[]=new int[8];
+        for(int i=0;i<array.length;i++)
+        {
+            array[i]=scanner.nextInt();
+        }
+
+        //int max = new ShengXuHe().max(array);
+        //System.out.println(max);
+    }
+}
